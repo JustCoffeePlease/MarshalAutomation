523246d7503e568de497c1920228333a3b87852b
Алексей <Vaas19.96@yandex.ru>
2023-02-10 15:19:22 +0300

    Seventh commit. Waiting for text to disappear

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -16,23 +17,45 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         // Подключение вебдрайвера
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Алексей\\Desktop\\chromedriver_win32\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("https://www.google.com/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        Actions actions = new Actions(driver);
-        WebElement searchField = driver.findElement(By.xpath("//input[@class='gLFyf']"));
+        try{
+            driver.get("https://pagination.js.org/");
+            Thread.sleep(2000);
 
-        searchField.sendKeys("auto", Keys.ENTER);
+            List<WebElement> elementList = (List<WebElement>) driver.findElements(By.xpath("//div[@class = 'data-container']/ul/li"));
+            List<WebElement> pagesList = (List<WebElement>) driver.findElements(By.xpath("//div[@class = 'paginationjs-pages']/ul/li"));
+            String text = elementList.get(5).getText();
 
-        WebElement resultCount = driver.findElement(By.id("result-stats"));
-        String resultParameter = resultCount.getText();
+            System.out.println(text);
 
-        System.out.println(resultParameter);
 
+            pagesList.get(2).click();
+            wait.until(ExpectedConditions.stalenessOf(elementList.get(5)));
+            // stalenessOf - ожидание, пока пропадет указанный элемент
+
+            elementList = (List<WebElement>) driver.findElements(By.xpath("//div[@class = 'data-container']/ul/li"));
+            text = elementList.get(5).getText();
+
+            System.out.println(text);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
      }
-
 }
+/*
+Созданы два листа:
+    elementList - информация объектов со страницы
+    pagesList - информация о страницах
+Таким образом, в начале информация об объектах первой страницы передается в elementList.
+После этого вызывается вторая страница. После вызова в elementList собирается информация со второй страницы.
+Чтобы вторая страница гарантированно успела подгрузиться, используется wait.until(ExpectedConditions.stalenessOf(elementList.get(5)));
+Перед переключем страницы и после переключения переменной text присваивается 5 элемент страницы. Переменная text выводится на экран
+*/
