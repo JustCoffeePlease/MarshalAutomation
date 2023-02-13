bd07f3000f966689f261f3615336b3b0bd61e93a
Алексей <Vaas19.96@yandex.ru>
2023-02-13 07:39:57 +0300

    Twelfth commit. Setting testNG

---
+++ src/main/java/TestOne.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestOne {
+
+    @Test
+    public void testOne() {
+        System.out.println("Test one");
+    }
+}
---
+++ src/main/java/TestTwo.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestTwo {
+
+    @Test
+    public void testTwo() {
+        System.out.println("Test two");
+    }
+}
