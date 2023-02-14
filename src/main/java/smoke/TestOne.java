bcb0bfb5891f50b42924b2234d498e12c522ca19
Алексей <Vaas19.96@yandex.ru>
2023-02-14 23:17:29 +0300

    Fortieth commit. Using parameters, using DataProvider

--- src/main/java/smoke/TestOne.java
+++ src/main/java/smoke/TestOne.java
@@ -1,23 +1,22 @@
 package smoke;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestOne {
 
-    @Test (groups = {"smokeTest", "regress"})
-    public void testOne() {
-        System.out.println("Test one");
+    @DataProvider(name = "dataBase")
+    public static Object[][] dataBase() {
+        return new Object[][] {
+                {"SQL", new Integer(1)},
+                {"NOSQL", new Integer(2)}
+        };
     }
 
-    @Test (groups = {"regress"})
-    public void testOneOne() {
-        System.out.println("Test one one");
+    @Parameters({"db"})
+    @Test (dataProvider = "dataBase")
+    public void testOne(String element1, int element2) {
+        System.out.println("Test one" + " " + element1 + " " + element2);
     }
-
-    @Test (groups = {"regress"})
-    public void testOneOneOne() {
-        System.out.println("Test one one one");
-    }
-
-
 }
