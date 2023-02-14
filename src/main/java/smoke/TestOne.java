7f5c72e85587ca4db4e847f461f265f20fa33a87
Алексей <Vaas19.96@yandex.ru>
2023-02-14 23:25:07 +0300

    Fiftieth commit. Interdependence of tests

--- src/main/java/smoke/TestOne.java
+++ src/main/java/smoke/TestOne.java
@@ -4,19 +4,23 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+/*Задача: организовать выполнение тестов таким образом,
+    чтобы второй тест запускался только
+    при условии выполнения первого теста*/
+
 public class TestOne {
 
-    @DataProvider(name = "dataBase")
-    public static Object[][] dataBase() {
-        return new Object[][] {
-                {"SQL", new Integer(1)},
-                {"NOSQL", new Integer(2)}
-        };
+    @Test
+    public void testOne() {
+        System.out.println("Test one");
     }
 
-    @Parameters({"db"})
-    @Test (dataProvider = "dataBase")
-    public void testOne(String element1, int element2) {
-        System.out.println("Test one" + " " + element1 + " " + element2);
+    @Test (
+            dependsOnMethods = {"testOne"},
+            alwaysRun = true)
+    public void testTwo() {
+        System.out.println("Test one");
     }
 }
+/*Аннотация alwaysRun означает,
+ что если первый тест упал, второй все равно выполняется*/
