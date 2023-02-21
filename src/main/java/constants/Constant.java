4c90be25e70ef7866b077345ea819b4aa7da735a
Алексей <Vaas19.96@yandex.ru>
2023-02-21 07:20:58 +0300

    Nineteenth commit. BasePageJava class created

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -4,5 +4,7 @@ public class Constant {
 
     public static class TimeOutVariable {
         public static final int IMPLICIT_WAIT = 4;
+        public static final int EXPLICIT_WAIT = 10;
+
     }
 }
---
+++ src/main/java/pages/base/BasePage.java
@@ -0,0 +1,31 @@
+package pages.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static constants.Constant.TimeOutVariable.EXPLICIT_WAIT;
+
+//Здесь описана общая логика,
+//которая потребуется в каждом классе
+public class BasePage {
+
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+//    Метод открытия странички
+    public void open(String url){driver.get(url);
+    }
+//    Логика для явных ожиданий видимости элемента
+    public WebElement waitElementIsVisible(WebElement element){
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT)).until(ExpectedConditions.visibilityOf(element));
+        return element;
+    }
+}
